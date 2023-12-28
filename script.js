1e9d44de36b619c7604e2a9ac726b9152a614c50
Soyoon Moon <soyoonmoon23@gmail.com>
2023-12-28 00:56:09 +0000

    need to fix the photos

---
+++ script.js
@@ -0,0 +1,18 @@
+const images = [
+  'profile_pic.jpg',
+  'intelligentia.JPG',
+  'strawberry.JPG',
+  // Add more images as needed
+];
+
+const imgElement = document.getElementById('main-photo');
+let currentImageIndex = 0;
+
+// Function to change the image
+function changeImage() {
+  imgElement.src = images[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % images.length; // Wrap around to the beginning
+}
+
+// Set an interval to change the image every second
+setInterval(changeImage, 1000);
